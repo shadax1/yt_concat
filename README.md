451282d339fb9563cdfcd20a6141f200ba0905ff
shadax1 <shadax1@protonmail.com>
2023-09-23 11:18:45 +0200

    v1.2
    added the possibility to concatenate twitch videos and clips + improved code in general

--- README.md
+++ README.md
@@ -1,41 +1,51 @@
 # yt_concat
-Python script that concatenates segments of YouTube videos together in either `mp3` or `mp4` format.
+Python script that concatenates segments of YouTube and Twitch videos together in either mp3 or mp4 format.
+It currently supports YouTube links (both mp3 and mp4), Twitch videos (both mp3 and mp4) and Twitch clips (only mp4).
 
 ## Requirements
 + Python 3
-+ `ffmpeg` which can be downloaded from [here](https://github.com/BtbN/FFmpeg-Builds/releases). The script assumes `ffmpeg` is in the `PATH` environment variables. Type `ffmpeg` in a command line to see if the command is found or not
-+ `yt-dlp` which can be downloaded from [here](https://github.com/yt-dlp/yt-dlp/releases). The script assumes `yt-dlp` is in the `PATH` environment variables. Type `yt-dlp` in a command line to see if the command is found or not
++ `ffmpeg` which can be downloaded [here](https://github.com/BtbN/FFmpeg-Builds/releases). Type `ffmpeg` in a command line to see if the command is found or not
++ `yt-dlp` which can be downloaded [here](https://github.com/yt-dlp/yt-dlp/releases). Type `yt-dlp` in a command line to see if the command is found or not
 
 This script used to use `youtube-dl` but it hasn't been maintained since December 2021 so `yt-dlp` is a good alternative.
 
 ## How to use
-Download this repository and unzip it anywhere on your disk.
+1. Download this repository and unzip it anywhere on your disk.
 
-Open `links.txt` and add either `mp3` or `mp4` in the first line based on the type of file you want.
+2. Open `links.txt` and add either `mp3` or `mp4` in the first line based on the type of file you want.
 
-Then, the next lines will each contain: youtube link, timestamp, duration of the clip from that timestamp
-Here is an example:
-```
-mp4
-https://www.youtube.com/watch?v=EhKXCrV2eJw 4:18 0:18
-```
-This means you will get an 18 second `mp4` clip starting from the 4:18 timestamp in the video. You can also use decimals to be more precise.
+3. In the next lines, add links, timestamp and duration separated by comas or the line can contain only a link for the entirety of the video/audio stream:
 
-Here is another example with multiple entries:
-```
-mp4
-https://www.youtube.com/watch?v=EhKXCrV2eJw 0:02.5 0:02.5
-https://www.youtube.com/watch?v=EhKXCrV2eJw 0:53.7 0:06
-https://www.youtube.com/watch?v=EhKXCrV2eJw 1:19.5 0:20
-https://www.youtube.com/watch?v=EhKXCrV2eJw 4:18 0:18
-https://www.youtube.com/watch?v=EhKXCrV2eJw 4:44 0:09
-https://www.youtube.com/watch?v=EhKXCrV2eJw 5:50 0:21
-```
-This will concatenate all these clips together into a single `mp4` file. You can also mix different video links. Note that if you plan to mix different links with the `mp4` tag, then their maximum resolutions need to be the same or it will create errors.
+    >`<youtube/twitch link>`, `<timestamp>`, `<duration of the clip from that timestamp>`
 
-Finally, open a command line and type:
-```
-python yt_concat.py
-```
+    >`<youtube/twitch link>`
 
-The resulting file will be named `output.mp3` or `output.mp4`. The script also saves each segment.
+    Here is an example:
+    ```
+    mp4
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 4:18 0:18
+    ```
+    This means you will get an 18 second `mp4` clip starting from the 4:18 timestamp in the video. You can also use decimals to be more precise.
+
+    Here is another example with multiple entries:
+    ```
+    mp4
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 0:02.5 0:02.5
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 0:53.7 0:06
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 1:19.5 0:20
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 4:18 0:18
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 4:44 0:09
+    https://www.youtube.com/watch?v=EhKXCrV2eJw 5:50 0:21
+    https://clips.twitch.tv/EntertainingCrackyDinosaurJebaited-B3L3T7XGxBsEU8rE
+    ```
+    This will concatenate all these clips together into a single `mp4` file. Notice how you can mix YouTube and Twitch links together.
+
+    ### Note regarding mixing links together
+    I have mainly done tests with links coming from the same channel but it's also possible to mix different video links from various channels. The main thing to take into account if that's what you want to do with the `mp4` tag in particular, then the videos' maximum resolutions should be the same or it will likely result in errors. Same goes for `mp3` as it can sometimes result in distorted audio.
+
+4. Finally, open a command line and type:
+    ```
+    python yt_concat.py
+    ```
+
+    The resulting file will be named `output_X.mp3` or `output_X.mp4` where X is an incrementing number in case other outputs exist in the folder. The script also saves each segment in the `segments` folder but deletes them at each runtime.
--- yt_concat.py
+++ yt_concat.py
@@ -1,26 +1,122 @@
 import os
 from subprocess import check_output
 
-script_path = os.path.dirname(os.path.abspath( __file__ ))
-extension = "mp"
+script_dir = os.path.dirname(__file__)
 
-with open(f"{script_path}\links.txt", mode="rt", encoding="utf-8") as fp:
-    count = 0
-    with open(f"{script_path}\concat.txt", mode="wt", encoding="utf-8") as fp2:
-        for line in fp.readlines():
-            if "mp4" in line:
-                extension = "mp4"
-            elif "mp3" in line:
-                extension = "mp3"
-            else:
-                lst_link = line.split()
-                response = check_output(f"yt-dlp -g {lst_link[0]}", shell=True)
-                output = response.decode("utf-8")
-                lst_output = output.split("\n")
-                if extension == "mp4":
-                    os.system(fr'ffmpeg -ss {lst_link[1]} -i "{lst_output[0]}" -ss {lst_link[1]} -i "{lst_output[1]}" -t {lst_link[2]} -map 0:v -map 1:a -c:v libx264 -c:a aac {script_path}\{count}.{extension}')
+def main():
+    extension = ""
+    count = 1
+    with open(f"{script_dir}{os.path.sep}links.txt", mode="rt", encoding="utf-8") as fp_links:
+        with open(f"{script_dir}{os.path.sep}concat.txt", mode="wt", encoding="utf-8") as fp_concat:
+            for line in fp_links.readlines():
+                #first line
+                if "mp4" in line:
+                    extension = "mp4"
+                    print("[SCRIPT] format is mp4")
+                elif "mp3" in line:
+                    extension = "mp3"
+                    print("[SCRIPT] format is mp3")
+
+                #after the first line
                 else:
-                    os.system(fr'ffmpeg -ss {lst_link[1]} -t {lst_link[2]} -i "{lst_output[1]}" {script_path}\{count}.{extension}')
-                fp2.write(f"file '{script_path}\{count}.{extension}'\n")
-            count += 1
-os.system(fr'ffmpeg -safe 0 -f concat -i {script_path}\concat.txt -c copy {script_path}\output.{extension}')
+                    if len(line.split()) == 3: #when a timestamp and duration is specified
+                        link = line.split()[0]
+                        timestamp = line.split()[1]
+                        length = line.split()[2]
+
+                        #get stream
+                        response = check_output(f"yt-dlp -g {link} --no-warnings", shell=True)
+                        url = response.decode("utf-8")
+                        video_stream = url.split("\n")[0] #youtube separates both audio and video
+                        audio_stream = url.split("\n")[1] #youtube separates both audio and video
+
+                        if extension == "mp4":
+                            if "youtu" in link: #youtube link
+                                get_yt_segment(extension, timestamp, length, video_stream, audio_stream, count)
+                                fp_concat.write(f"file '{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}'\n")
+                            elif "twitch" in link: #twitch link
+                                get_twitch_segment(extension, timestamp, length, url, count)
+                                fp_concat.write(f"file '{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}'\n")
+                            else:
+                                print(f"[WARNING] link {link} is unsupported")
+                        else:
+                            if "youtu" in link: #youtube link
+                                get_yt_segment(extension, timestamp, length, video_stream, audio_stream, count)
+                                fp_concat.write(f"file '{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}'\n")
+                            elif "twitch" in link: #twitch link
+                                if "clip" not in link:
+                                    get_twitch_segment(extension, timestamp, length, url, count)
+                                    fp_concat.write(f"file '{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}'\n")
+                                else:
+                                    print(f"[WARNING] can't extract an audio segment from a twitch clip... --> {link}")
+                            else:
+                                print(f"[WARNING] link {link} is unsupported")
+                    
+                    elif len(line.split()) == 1: #no timestamp/duration specified -> download the whole thing
+                        link = line
+                        get_full(extension, link, count)
+                        fp_concat.write(f"file '{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}'\n")
+                    else:
+                        print(f"[WARNING] information missing in line --> {line}")
+                    count += 1
+
+    print("[INFO] concatenating all segments...")
+    if not os.path.exists(f"{script_dir}{os.path.sep}output.{extension}"):
+        final_file_name = f"{script_dir}{os.path.sep}output.{extension}"
+    else:
+        bool_saved = False
+        count_filename = 1
+        while not bool_saved:
+            if os.path.exists(f"{script_dir}{os.path.sep}output_{count_filename}.{extension}"):
+                count_filename += 1
+            else:
+                final_file_name = f"{script_dir}{os.path.sep}output_{count_filename}.{extension}"
+                bool_saved = True
+    os.system(fr'ffmpeg -safe 0 -f concat -i "{script_dir}{os.path.sep}concat.txt" -c copy {final_file_name} -loglevel error -stats')
+    print(fr"[INFO] concatenated file saved under '{final_file_name}'")
+
+#FFMPEG - YT-DLP
+def get_yt_segment(extension, timestamp, length, video_stream, audio_stream, count):
+    print(f"[SCRIPT] entry {count} is a youtube segment")
+    if extension == "mp4":
+        os.system(fr'ffmpeg -ss "{timestamp}" -i "{video_stream}" -ss "{timestamp}" -i "{audio_stream}" -t "{length}" -map 0:v -map 1:a -c:v libx264 -c:a aac "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -loglevel error -stats')
+    elif extension == "mp3":
+        os.system(fr'ffmpeg -ss "{timestamp}" -t "{length}" -i "{audio_stream}" "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -loglevel error -stats')
+
+def get_twitch_segment(extension, timestamp, length, url, count):
+    print(f"[SCRIPT] entry {count} is a twitch segment")
+    if extension == "mp4":
+        os.system(fr'ffmpeg -ss "{timestamp}" -i "{url[:-1]}" -t "{length}" "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -loglevel error -stats') #url[:-1] to remove the \n
+    elif extension == "mp3":
+        os.system(fr'ffmpeg -ss "{timestamp}" -i "{url[:-1]}" -t "{length}" -map 0:a "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -loglevel error -stats') #url[:-1] to remove the \n
+
+def get_full(extension, link, count):
+    if extension == "mp4":
+        if "youtu" in link: #youtube link
+            print(f"[SCRIPT] entry {count} is a full youtube video")
+        elif "twitch" in link: #twitch link
+            if "clip" not in link:
+                print(f"[SCRIPT] entry {count} is a full twitch video")
+            else:
+                print(f"[SCRIPT] entry {count} is a twitch clip")
+        os.system(fr'yt-dlp --output "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --quiet --progress {link}')
+    
+    elif extension == "mp3":
+        if "clip" in link: #condition only to display the following message
+            print(f"[INFO] can't extract mp3 from a twitch clip... --> {link}")
+        else:
+            if "youtu" in link: #youtube link
+                print(f"[SCRIPT] entry {count} is a full youtube audio")
+            elif "twitch" in link and "clip" not in link: #twitch link
+                print(f"[SCRIPT] entry {count} is a full twitch audio")
+            os.system(fr'yt-dlp --output "{script_dir}{os.path.sep}segments{os.path.sep}{count}.{extension}" -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --quiet --progress {link}')
+    else:
+        print(f"[WARNING] link {link} is unsupported")
+
+def empty_segments_folder():
+    path = fr'{script_dir}{os.path.sep}segments'
+    for f in os.listdir(path):
+        os.remove(fr"{path}{os.path.sep}{f}")
+
+empty_segments_folder()
+main()
